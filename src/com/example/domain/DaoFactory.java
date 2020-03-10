9e5c25afb10fdc49e48dec0df5cabd74a20091f5
MoonJiHyun <boo74153@naver.com>
2020-03-10 21:35:50 +0900

    [1.8.2] xml을 이용하는 애플리케이션 컨텍스트

--- src/com/example/domain/DaoFactory.java
+++ src/com/example/domain/DaoFactory.java
@@ -3,16 +3,18 @@ package com.example.domain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class DaoFactory {
-
-    @Bean
-    public UserDao userDao() {
-        return new UserDao(connectionMaker());
-    }
-
-    @Bean
-    public ConnectionMaker connectionMaker() {
-        return new DConnectionMaker();
-    }
-}
+//@Configuration
+//public class DaoFactory {
+//
+//    @Bean
+//    public UserDao userDao() {
+//        UserDao userDao = new UserDao();
+//        userDao.setConnectionMaker(connectionMaker());
+//        return userDao;
+//    }
+//
+//    @Bean
+//    public ConnectionMaker connectionMaker() {
+//        return new DConnectionMaker();
+//    }
+//}
