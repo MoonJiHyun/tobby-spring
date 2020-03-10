05b729fd1bd1e44be17958404986791196473984
MoonJiHyun <boo74153@naver.com>
2020-03-10 21:24:23 +0900

    [1.7.4] CountingConnectionMaker

---
+++ src/com/example/domain/UserDaoConnectionCountingTest.java
@@ -0,0 +1,15 @@
+package com.example.domain;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoConnectionCountingTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println(ccm.getCounter());
+    }
+}
--- src/com/example/domain/UserDaoTest.java
+++ src/com/example/domain/UserDaoTest.java
@@ -6,36 +6,23 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.sql.SQLException;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회 성공");
-
-        DaoFactory factory = new DaoFactory();
-        UserDao dao1 = factory.userDao();
-        UserDao dao2 = factory.userDao();
-
-        System.out.println(dao1);
-        System.out.println(dao2);
-
-        UserDao dao3 = context.getBean("userDao", UserDao.class);
-        UserDao dao4 = context.getBean("userDao", UserDao.class);
-
-        System.out.println(dao3);
-        System.out.println(dao4);
-    }
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        UserDao dao = context.getBean("userDao", UserDao.class);
+//
+//        User user = new User();
+//        user.setId("whiteship");
+//        user.setName("백기선");
+//        user.setPassword("married");
+//
+//        dao.add(user);
+//
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//        User user2 = dao.get(user.getId());
+//
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//        System.out.println(user2.getId() + " 조회 성공");
+//    }
 }
