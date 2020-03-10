3727e6261c4221cd0e34a69ae71aeb09ff2ff04c
MoonJiHyun <boo74153@naver.com>
2020-03-10 19:40:37 +0900

    [1.3.2] 관계설정 책임의 분리

--- src/com/example/domain/UserDaoTest.java
+++ src/com/example/domain/UserDaoTest.java
@@ -2,10 +2,10 @@ package com.example.domain;
 
 import java.sql.SQLException;
 
-public class Main {
-
+public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao dao = new UserDao(connectionMaker);
 
         User user = new User();
         user.setId("whiteship");
@@ -17,6 +17,7 @@ public class Main {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
+        
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
         System.out.println(user2.getId() + " 조회 성공");
