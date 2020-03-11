8fb762b4af339adda198e9cec2075f5341aafbb0
MoonJiHyun <boo74153@naver.com>
2020-03-11 23:14:12 +0900

    [2.3.3] get() 테스트 기능을 보완한 addAndGet() 테스트

--- src/com/example/domain/UserDaoTest.java
+++ src/com/example/domain/UserDaoTest.java
@@ -15,18 +15,23 @@ public class UserDaoTest {
     public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("com/example/domain/applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user = new User("gyumee", "박성철", "springno1");
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        dao.add(user);
-        assertThat(dao.getCount(), is(1));
-        User user2 = dao.get(user.getId());
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
     }
 
     @Test
@@ -49,6 +54,5 @@ public class UserDaoTest {
 
         dao.add(user3);
         assertThat(dao.getCount(), is(3));
-
     }
 }
