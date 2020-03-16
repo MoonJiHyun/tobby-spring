dfdcafa9f1a3b88b6b54a581ccda23ec97541d15
MoonJiHyun <boo74153@naver.com>
2020-03-16 23:18:18 +0900

    [2.5.2] JUnit 테스트 오브젝트 생성에 대한 학습 테스트

---
+++ src/com/example/domain/JUnitTest.java
@@ -0,0 +1,30 @@
+package com.example.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsSame.sameInstance;
+import static org.junit.Assert.assertThat;
+
+public class JUnitTest {
+    static JUnitTest testObject;
+
+    @Test
+    public void test1() {
+        assertThat(this, is(not(sameInstance(testObject))));
+        testObject = this;
+    }
+
+    @Test
+    public void test2() {
+        assertThat(this, is(not(sameInstance(testObject))));
+        testObject = this;
+    }
+
+    @Test
+    public void test3() {
+        assertThat(this, is(not(sameInstance(testObject))));
+        testObject = this;
+    }
+}
